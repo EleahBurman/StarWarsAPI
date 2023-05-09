8fd319e23ee43b785e93caff3055cd2abc9ad202
EleahBurman <eleahjburman@gmail.com>
2023-05-09 09:38:08 -0400

    Add width and height to landing page star wars

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 const Landing = () => {
   return (
     <div className="landing">
-      <img src="http://imageshack.com/a/img922/3783/oyvsRd.png" alt="Star Wars Landing Main Photo" />
+      <img src="http://imageshack.com/a/img922/3783/oyvsRd.png" alt="Star Wars Landing Main Photo" style={{width: 695, height: 420 }}/>
     </div>
   )
 }
